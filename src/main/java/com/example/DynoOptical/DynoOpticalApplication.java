926c52b8ae76b674a8df6a1c4825197df7770bd8
sgurung183 <gurungsushant30@gmail.com>
2025-07-11 15:22:19 -0700

    initial Commit

---
+++ src/main/java/com/example/DynoOptical/DynoOpticalApplication.java
@@ -0,0 +1,13 @@
+package com.example.DynoOptical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynoOpticalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynoOpticalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DynoOptical/DynoOpticalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DynoOptical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynoOpticalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
